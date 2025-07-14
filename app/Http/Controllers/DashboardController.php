231edc356311904c05834e2f7333564b00bef67c
Your Name <you@example.com>
2025-07-14 23:21:21 +0300

    first commit2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Land;
+use App\Models\Building;
+use App\Models\Landmark;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+
+    $buildingsCount = Building::count();
+    $landsCount = Land::count();
+    $landmarksCount = Landmark::count();
+
+        return view('dashboard',
+        compact('buildingsCount','landsCount','landmarksCount'));
     }
 }
